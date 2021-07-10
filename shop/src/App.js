3547e1b06b27a84b55426976514425e1058814a5
joonayd <63727069+joonayd@users.noreply.github.com>
2021-07-10 18:09:30 +0500

    uwu

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import TopMenu from "./components/TopMenu";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
+import Products from "./components/products/Products";
+import ContactUs from "./components/ContactUs";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
@@ -9,7 +12,11 @@ function App() {
       <div>
         <TopMenu />
         <Switch>
-          <Route to="/" component={LandingPage} />
+          <Route path="/products" component={Products} />
+          <Route path="/contact-us" component={ContactUs} />
+          <Route path="/not-found" component={NotFound} />
+          <Route path="/" exact component={LandingPage} />
+          <Redirect to="/not-found" />
         </Switch>
       </div>
     </Router>
--- shop/src/components/TopMenu.jsx
+++ shop/src/components/TopMenu.jsx
@@ -1,17 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 
 const TopMenu = () => {
   return (
     <div>
       <ul>
-        <li>
-          <a href="/">Home</a>
+        <li style={{ display: "inline", padding: "5px" }}>
+          <Link to="/">Home</Link>
         </li>
-        <li>
-          <a href="/products">Products</a>
+        <li style={{ display: "inline", padding: "5px" }}>
+          <Link to="/products">Products</Link>
         </li>
-        <li>
-          <a href="/products">Contact Us</a>
+        <li style={{ display: "inline", padding: "5px" }}>
+          <Link to="/contact-us">Contact Us</Link>
+        </li>
+        <li style={{ display: "inline", padding: "5px" }}>
+          <Link to="/login">Login</Link>
+        </li>
+        <li style={{ display: "inline", padding: "5px" }}>
+          <Button color="secondary">Signup</Button>
         </li>
       </ul>
     </div>
